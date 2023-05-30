731f7db9f74854b9b78a4dc9e1439e3a8dbaf026
Ivan Slukin <provaider010@gmail.com>
2023-05-30 19:39:52 +1000

    neveroyatni commit

---
+++ DZ_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+  int sum(int N)
+  {
+    int counter = Convert.ToString(N).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = N - N % 10;
+      result = result + (N - advance);
+      N = N / 10;
+    }
+   return result;
+  }
+
+int Sum = sum(N);
+Console.WriteLine("Сумма цифр в числе: " + Sum);
